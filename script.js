4cec9db9cf2baf3c93c5fa6771ed96e5a9ab7d23
Awais Raees Ansari <80111573+AnsariAwaisRaees@users.noreply.github.com>
2023-11-28 13:31:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 const makeChange = (c) => {
   // your name here
-	let obj = {
-		let q=0, d=0, n=0, p=0;
-		obj['q'] = parseInt(c/25);
-		c = c%25;
-		obj['d'] = parseInt(c/10);
-		c = c%10;
-		obj['n'] = parseInt(c/5);
-		c = c%5;
-		obj['p'] = c;
+	let obj = {}
+	let q=0, d=0, n=0, p=0;
+	obj['q'] = parseInt(c/25);
+	c = c%25;
+	obj['d'] = parseInt(c/10);
+	c = c%10;
+	obj['n'] = parseInt(c/5);
+	c = c%5;
+	obj['p'] = c;
 
-		return obj;
-	}
+	return obj;
+	
 };
 
 // Do not the change the code below
